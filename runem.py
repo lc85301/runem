e3fdff0b331d7fa0c7b60f0cae93042e53587b54
yodalee <lc85301@gmail.com>
2013-07-06 21:35:31 +0000

    add monitor notify, and grep pid

--- runem.py
+++ runem.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python
 
-##############################################
-# File: runem.py
-# Description: the automatic run em
-##############################################
+##################################################################################
+#-*- coding: utf-8 -*-
+# 
+# Filename: runem.py
+#
+# Copyright (C) 2013 -  You-Tang Lee (YodaLee) <lc85301@gmail.com>
+# All Rights reserved.
+#
+# This file is part of project: runem.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##################################################################################
 
 import sys
 import os.path
 import subprocess
 import shutil
 import smtplib
+import getpass
 from email.mime.text import MIMEText
 import re
+
+# parse option 
 #from optparse import OptionParser,make_option
 #
 #option_list = [
-#	make_option("-f", "--file", action='store',dest='file', default=False)
+#	make_option("-s", "--set", action='store',dest='file', default=False)
 #]
 #parser = OptionParser(usage = 'Usage: autoEM [OPTION...] DATA...', option_list=option_list)
 #
@@ -38,7 +61,7 @@ def getmail():
 			mailfile.close()
 	return mail
 
-def sendmail():
+def sendmail(filename):
 	"""get mail then sendmail"""
 	sender = "lc85301@gmail.com"
 	receiver = getmail()
@@ -46,15 +69,15 @@ def sendmail():
 	if receiver == "":
 		return 0
 	# open smtp session
-	session  = smtplib.SMTP('smtp.gmail.com', 587)
+	session = smtplib.SMTP('smtp.gmail.com', 587)
 	session.ehlo
 	session.starttls()
 	session.ehlo
 	session.login(account, password)
 
 	# prepare the send message
-	msg = MIMEText("smtp module test")
-	msg['Subject'] = "the test title"
+	msg = MIMEText("Your EM simulation of {0} is over. Have a good day.".format())
+	msg['Subject'] = "EM simulation is over"
 	msg['From'] = sender
 	msg['To'] = receiver
 
@@ -62,31 +85,61 @@ def sendmail():
 	session.sendmail(sender, [receiver], msg.as_string())
 	session.quit()
 
-sendmail()
-filename = sys.argv[1]
+def monitor_pid(pid):
+	"""Monitor a pid, then send message"""
+	sendmail(filename)
 
-if not filename.endswith(".son"):
-	sys.stderr.write("not a valid sonnet simulation file\n")
-	sys.exit(0)
+def getmail():
+	"""allow user input mail and save into ~/.runem"""
+	setting = ".runem"
+	mail = input("Please input your email address: ")
+	filepath = os.path.expanduser("~") + "/" + setting
+	mailfile = None
+	try:
+		mailfile = open(filepath , mode="w")
+		mailfile.write(mail)
+	except IOError:
+		pass
+	finally:
+		if mailfile is not None:
+			mailfile.close()
 
-if not os.path.isfile(filename):
-	sys.stderr.write("the simulation file doesn't exist\n")
-	sys.exit(0)
-#the file path of the simulation file
-pathname = os.path.dirname(os.path.abspath(filename))
-rawfilename = filename[:-4]
-print(pathname)
-print(rawfilename)
+def getpid():
+	"""using ps to find the em simulation pid just called"""
+	username = getusername()
+	command = "ps -elf ｜ grep 'em -v' | grep {0} | grep [^grep] ｜ tr -s ' ' | cut -d ' ' -f 4 | sort -v".format(username)
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+	print(p.stdout)
+	return pid
 
-# if exist, remove the old running log directory
-# otherwise em will stop simulation
-setting="{0}/sondata/{1}".format(pathname, rawfilename)
-if os.path.isdir(setting):
-	shutil.rmtree(setting)
+def getusername():
+	"""get user name, to grep em process"""
+	return getpass.getuser()
 
-# now run the em simulation using em and "at" command
-#command = ["echo","-v {0}".format(filename),"|", "at", "now +1 minutes"]
-command = "echo 'em -v {0}' | at now +1 minutes".format(filename)
-p  = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-p_stdout = p.stdout.read()
-p_stderr = p.stderr.read()
+#filename = sys.argv[1]
+
+#if not filename.endswith(".son"):
+#	sys.stderr.write("not a valid sonnet simulation file\n")
+#	sys.exit(0)
+#
+#if not os.path.isfile(filename):
+#	sys.stderr.write("the simulation file doesn't exist\n")
+#	sys.exit(0)
+##the file path of the simulation file
+#pathname = os.path.dirname(os.path.abspath(filename))
+#rawfilename = filename[:-4]
+#print(pathname)
+#print(rawfilename)
+#
+## if exist, remove the old running log directory
+## otherwise em will stop simulation
+#setting="{0}/sondata/{1}".format(pathname, rawfilename)
+#if os.path.isdir(setting):
+#	shutil.rmtree(setting)
+#
+## now run the em simulation using em and "at" command
+##command = ["echo","-v {0}".format(filename),"|", "at", "now +1 minutes"]
+#command = "echo 'em -v {0}' | at now +1 minutes".format(filename)
+#p  = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#p_stdout = p.stdout.read()
+#p_stderr = p.stderr.read()
